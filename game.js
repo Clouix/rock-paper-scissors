21778d7a9c3899dec488534cf9600a1ab403b879
Ale Borge <clouix23@gmail.com>
2023-07-31 16:54:51 -0600

    change on user game choice

--- game.js
+++ game.js
@@ -9,21 +9,43 @@ console.log(getComputerChoice())
 function playRound(playerSelection, computerSelection) {
     let winnerMessage = "";
     if(playerSelection === computerSelection) {
-        winnerMessage = "It's a Tie! " + playerSelection + " can't beat " + computerSelection
+        winnerMessage = "It's a Tie! " + playerSelection + " can't beat " + computerSelection;
     } else if(playerSelection === "rock" && computerSelection === "scissors") {
-        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection
+        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
     } else if(playerSelection === "paper" && computerSelection === "rock") {
-        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection
+        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
     } else if(playerSelection === "scissors" && computerSelection === "paper") {
-        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection
+        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
     } else {
-        winnerMessage = "You Lose! " + computerSelection + " beats " + playerSelection
+        winnerMessage = "You Lose! " + computerSelection + " beats " + playerSelection;
     }
 
     return winnerMessage
 }
 
-let playerSelection = "RoCK";
-playerSelection = playerSelection.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    let palyerScore;
+    let computerScore;
+    let userOption;
+    const computerSelection = getComputerChoice();
+
+function game() {
+    
+    
+    for(let i = 0; i < 5; i++) {
+        
+        userChoice();
+    }
+}
+
+function userChoice() {
+    userOption = prompt("Choose between: rock, paper or scissors!");
+    userOption = userOption.toLowerCase();
+    if(userOption == 'rock' || userOption == 'paper' || userOption == 'scissors') {
+        console.log(playRound(userOption, computerSelection));
+    } else {
+        alert('Invalid choice, write a valid one');
+        userChoice();
+    }
+}
+
+console.log(game())
