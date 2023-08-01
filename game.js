11e2cf2098fe5eda1acbe0eb19e41e2e8829fa9d
Ale Borge <clouix23@gmail.com>
2023-07-31 19:39:34 -0600

    score and winner message

--- game.js
+++ game.js
@@ -1,40 +1,47 @@
+let playerScore = 0;
+let computerScore = 0;
+let userOption;
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const random_choice = choices[Math.floor(Math.random() * choices.length)];
     return random_choice
 }
 
-console.log(getComputerChoice())
-
 function playRound(playerSelection, computerSelection) {
     let winnerMessage = "";
     if(playerSelection === computerSelection) {
         winnerMessage = "It's a Tie! " + playerSelection + " can't beat " + computerSelection;
-    } else if(playerSelection === "rock" && computerSelection === "scissors") {
-        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
-    } else if(playerSelection === "paper" && computerSelection === "rock") {
-        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
-    } else if(playerSelection === "scissors" && computerSelection === "paper") {
-        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
+    } else if(playerSelection === "rock" && computerSelection === "scissors" || 
+              playerSelection === "paper" && computerSelection === "rock" ||
+              playerSelection === "scissors" && computerSelection === "paper") {
+                playerScore = ++playerScore;
+                winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
     } else {
+        computerScore = ++computerScore;
         winnerMessage = "You Lose! " + computerSelection + " beats " + playerSelection;
     }
 
     return winnerMessage
 }
 
-    let palyerScore;
-    let computerScore;
-    let userOption;
-    const computerSelection = getComputerChoice();
-
 function game() {
-    
-    
     for(let i = 0; i < 5; i++) {
-        
         userChoice();
+        console.log("User points: " + userPoints() + " vs Computer points: " + pcPoints());
     }
+    console.log(winner())
+}
+
+function userPoints() {
+    let userScore = playerScore;
+    return userScore;
+}
+
+function pcPoints() {
+    let aiScore = computerScore;
+    return aiScore;
 }
 
 function userChoice() {
@@ -46,6 +53,18 @@ function userChoice() {
         alert('Invalid choice, write a valid one');
         userChoice();
     }
+}
+
+function winner() {
+    let gameWinner = '';
+    if(userPoints() == pcPoints()) {
+        gameWinner = "This was a close game. Tie!";
+    } else if(userPoints() > pcPoints()) {
+        gameWinner = "Congratulations the user wins!";
+    } else {
+        gameWinner = "Best luck the next time. Computer wins this one!";
+    }
+    return gameWinner;
 }
 
 console.log(game())
