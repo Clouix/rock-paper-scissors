6032bbfb57b925a36010211ae81725986f58bee1
Ale Borge <clouix23@gmail.com>
2023-09-29 22:53:23 -0600

    change so the ai select a different option  on every round

--- game.js
+++ game.js
@@ -1,54 +1,53 @@
 let playerScore = 0;
 let computerScore = 0;
 let userOption;
-const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const random_choice = choices[Math.floor(Math.random() * choices.length)];
-    return random_choice
+    return random_choice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
     let winnerMessage = "";
-    if(playerSelection === computerSelection) {
+
+    if (playerSelection === computerSelection) {
         winnerMessage = "It's a Tie! " + playerSelection + " can't beat " + computerSelection;
-    } else if(playerSelection === "rock" && computerSelection === "scissors" || 
-              playerSelection === "paper" && computerSelection === "rock" ||
-              playerSelection === "scissors" && computerSelection === "paper") {
-                playerScore = ++playerScore;
-                winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore = ++playerScore;
+        winnerMessage = "You Won! " + playerSelection + " beats " + computerSelection;
     } else {
         computerScore = ++computerScore;
         winnerMessage = "You Lose! " + computerSelection + " beats " + playerSelection;
     }
 
-    return winnerMessage
+    return winnerMessage;
 }
 
 function game() {
-    for(let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
         userChoice();
         console.log("User points: " + userPoints() + " vs Computer points: " + pcPoints());
     }
-    console.log(winner())
+    console.log(winner());
 }
 
 function userPoints() {
-    let userScore = playerScore;
-    return userScore;
+    return playerScore;
 }
 
 function pcPoints() {
-    let aiScore = computerScore;
-    return aiScore;
+    return computerScore;
 }
 
 function userChoice() {
     userOption = prompt("Choose between: rock, paper or scissors!");
     userOption = userOption.toLowerCase();
-    if(userOption == 'rock' || userOption == 'paper' || userOption == 'scissors') {
-        console.log(playRound(userOption, computerSelection));
+    if (userOption == 'rock' || userOption == 'paper' || userOption == 'scissors') {
+        console.log(playRound(userOption));
     } else {
         alert('Invalid choice, write a valid one');
         userChoice();
@@ -57,14 +56,14 @@ function userChoice() {
 
 function winner() {
     let gameWinner = '';
-    if(userPoints() == pcPoints()) {
+    if (userPoints() == pcPoints()) {
         gameWinner = "This was a close game. Tie!";
-    } else if(userPoints() > pcPoints()) {
+    } else if (userPoints() > pcPoints()) {
         gameWinner = "Congratulations the user wins!";
     } else {
-        gameWinner = "Best luck the next time. Computer wins this one!";
+        gameWinner = "Best of luck next time. Computer wins this one!";
     }
     return gameWinner;
 }
 
-console.log(game())
+game();
